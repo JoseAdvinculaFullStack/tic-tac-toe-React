610572cec751ae08474648295dfebf78f80ec5e5
JoseAdvinculaFullStack <JoseAdvinculaFullStack@gmail.com>
2023-03-16 21:34:16 -0500

    arreglo

--- src/ButtonSelectPlayer/index.js
+++ src/ButtonSelectPlayer/index.js
@@ -1,16 +1,21 @@
 import styles from "./style.module.css";
 import Shape2 from "../assets/iconsSvg/Shape2";
 import Oval2 from "../assets/iconsSvg/Oval2";
-import { useState } from "react";
-const ButtonSelectPlayer = ({ type }) => {
-  const [firstPick, setFirstPick] = useState("active_x");
+
+const ButtonSelectPlayer = ({
+  type,
+  buttonStylePlayer,
+  buttonStyleCPU,
+  setButtonStylePlayer,
+  setButtonStyleCPU,
+}) => {
   const typebutton = type === "oval" ? <Oval2 /> : <Shape2 fill="#a8bfc9" />;
   const typeclass = type === "oval" ? "oval2" : "shape2";
   return (
     <button
-      className={`${styles.containerButtonPlayer} ${styles[typeclass]} ${styles[firstPick]}`}
+      className={`${styles.containerButtonPlayer} ${styles[typeclass]} `}
       type="button"
-      onClick={() => setFirstPick("active_o")}
+      onClick={() => "hola"}
     >
       {typebutton}
     </button>
--- src/Game/index.js
+++ src/Game/index.js
@@ -2,15 +2,30 @@ import styles from "./style.module.css";
 import Brand from "../Brand/index";
 import ButtonSelectPlayer from "../ButtonSelectPlayer";
 import ButtonOponent from "../ButtonOponent";
+import { useState } from "react";
 const Game = () => {
+  const [buttonStylePlayer, setButtonStylePlayer] = useState("");
+  const [buttonStyleCPU, setButtonStyleCPU] = useState("");
   return (
     <div className={styles.containerGame}>
       <Brand />
       <div className={styles.containerPickPlayer}>
         <h2>PICK PLAYER 1’S MARK</h2>
         <div className={styles.containerButtonsSelect}>
-          <ButtonSelectPlayer type="shape" />
-          <ButtonSelectPlayer type="oval" />
+          <ButtonSelectPlayer
+            type="shape"
+            buttonStylePlayer={buttonStylePlayer}
+            buttonStyleCPU={buttonStyleCPU}
+            setButtonStylePlayer={setButtonStylePlayer}
+            setButtonStyleCPU={setButtonStyleCPU}
+          />
+          <ButtonSelectPlayer
+            type="oval"
+            buttonStylePlayer={buttonStylePlayer}
+            buttonStyleCPU={buttonStyleCPU}
+            setButtonStylePlayer={setButtonStylePlayer}
+            setButtonStyleCPU={setButtonStyleCPU}
+          />
         </div>
         <h3>REMEMBER : X GOES FIRST</h3>
       </div>
